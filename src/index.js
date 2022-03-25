f0c13f5074ca64f52d8cd2b779c9ca80f98f21e8
Claudia-Fernanda-Rejas-Ruiz <claudia.rejas@usil.pe>
2022-03-25 04:52:27 -0500

    Se agregaron más funciones async para devolver datos

--- src/index.js
+++ src/index.js
@@ -165,3 +165,120 @@ async function getTopPopLatinos() {
       return []
   }
 }
+async function getTopArtistas() {
+  try {
+      let results = []
+      const response = await fetch("https://shazam.p.rapidapi.com/search?term=top%20artista", {
+          "method": "GET",
+          "headers": {
+              "x-rapidapi-host": "shazam.p.rapidapi.com",
+              "x-rapidapi-key": "9788db139bmshbc53a57232bd948p1e485fjsnf528f39d11ed"
+          }
+      })
+      const data = await response.json()
+      const artistas = data.artists.hits.filter(artista => artista.artist.avatar)
+      artistas.forEach(artista => {
+          results = [
+              ...results,
+              {
+                  title: artista.artist.name,
+                  image: artista.artist.avatar,
+                  url: artista.artist.weburl
+              }
+          ]
+      })
+      return results
+  } catch (err) {
+      console.log(err)
+      return []
+  }
+}
+// Para año nuevo
+async function getListRecommendations() {
+  try {
+      let listMusic = []
+
+      const response = await fetch("https://shazam.p.rapidapi.com/songs/list-recommendations?key=484129036&locale=en-US", {
+          "method": "GET",
+          "headers": {
+              "x-rapidapi-host": "shazam.p.rapidapi.com",
+              "x-rapidapi-key": "9788db139bmshbc53a57232bd948p1e485fjsnf528f39d11ed"
+          }
+      })
+      const data = await response.json()
+      data.tracks.forEach(track => {
+          listMusic = [
+              ...listMusic,
+              {
+                  title: track.title,
+                  subtitle: track.subtitle,
+                  image: track.share.image,
+                  url: track.url,
+              }
+          ]
+      })
+      return listMusic
+  } catch (err) {
+      console.log(err)
+      return []
+  }
+}
+// Para musica nueva 
+async function getNewMusicTracks() {
+  try {
+      let newMusic = []
+      const response = await fetch("https://shazam.p.rapidapi.com/search?term=new%20music", {
+          "method": "GET",
+          "headers": {
+              "x-rapidapi-host": "shazam.p.rapidapi.com",
+              "x-rapidapi-key": "9788db139bmshbc53a57232bd948p1e485fjsnf528f39d11ed"
+          }
+      })
+      const data = await response.json()
+
+      data.tracks.hits.forEach(hit => {
+          newMusic = [
+              ...newMusic,
+              {
+                  title: hit.track.title,
+                  subtitle: hit.track.subtitle,
+                  image: hit.track.share.image,
+                  url: hit.track.url
+              }
+          ]
+      })
+      return newMusic
+  } catch (err) {
+      console.log(err)
+      return []
+  }
+}
+// Para lista de canciones nuevas
+async function getListArtistTop() {
+  try {
+      let listArtistas = []
+      const response = await fetch("https://shazam.p.rapidapi.com/songs/list-artist-top-tracks?id=40008598&locale=en-US", {
+          "method": "GET",
+          "headers": {
+              "x-rapidapi-host": "shazam.p.rapidapi.com",
+              "x-rapidapi-key": "3edd3ae533msh440d6db57d7da46p10a811jsn03f9fa96060f"
+          }
+      })
+      const data = await response.json()
+      data.tracks.forEach(track => {
+          listArtistas = [
+              ...listArtistas,
+              {
+                  title: track.title,
+                  subtitle: track.subtitle,
+                  image: track.share.image,
+                  url: track.url,
+              }
+          ]
+      })
+      return listArtistas
+  } catch (err) {
+      console.log(err)
+      return []
+  }
+}
