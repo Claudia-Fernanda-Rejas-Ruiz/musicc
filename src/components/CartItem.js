b2cf61f53f6c9cea635ef187e516125e1463919c
Claudia-Fernanda-Rejas-Ruiz <claudia.rejas@usil.pe>
2022-03-28 08:11:43 -0500

    Se añadió conexiones entre botones

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,15 +1,15 @@
 
 const CartItem = ({ data, delFromCart }) => {
-    let { id, price, img, quantity } = data;
+    let { id, name, price, img, quantity } = data;
   
     return (
       
-            <article class="col-md-5 justify-content-center align-self-center">
+            <article class="col-md-5 p-5 row justify-content-center align-self-center">
                 <div class="card text-center text-white bg-dark justify-content-center align-self-center" id="musicCard">
                     <img src={img} class="card-img-top img-thumbnail img-fluid mx-auto" alt="Portada de tarjeta"></img>
                     <div class="card-body">
-                      <h3 class="card-title">Plan individual</h3>
-                      <p class="card-text">${price} x {quantity} = ${price * quantity}</p>
+                      <h3 class="card-title">{name}</h3>
+                      <p class="card-text">s/.{price} x {quantity} = s/.{price * quantity}</p>
                     </div>
                     <div class="btn-group" role="group" aria-label="Basic checkbox toggle button group">
                         <button class="btn btn-outline-primary" onClick={() => delFromCart(id)}>Eliminar Uno</button>
